973b8b18d380f47dcd3f18a8c3721323b30810ef
Rowan Krishnan <rkrishnan8594@gmail.com>
2015-12-21 14:24:42 -0500

    Some changes

--- src/js/taskbar.js
+++ src/js/taskbar.js
@@ -1,5 +1,25 @@
 function Taskbar() {
-  console.log('taskbar created');
+  this.init();
+};
+
+Taskbar.prototype.init = function() {
+  var template = Portfolio.templates.taskbar();
+  $('.taskbar').html(template);
+  this.bindHandlers();
+};
+
+Taskbar.prototype.addItem = function() {
+  console.log("Adding item to taskbar");
+};
+
+Taskbar.prototype.bindHandlers = function() {
+  var self = this;
+  console.log("Binding shit");
+  $('.taskbar__start-btn').on('click', self.openStartMenu)
+};
+
+Taskbar.prototype.openStartMenu = function() {
+  console.log("opening start menu");
 }
 
 new Taskbar();
--- src/js/templates.js
+++ src/js/templates.js
@@ -13,7 +13,7 @@ this["Portfolio"]["templates"]["desktop"] = Handlebars.template({"1":function(de
 
   return "<ul class=\"desktop__icns\">\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "</ul>\n\n<div class=\"taskbar\"></div>\n";
+    + "</ul>\n";
 },"useData":true});
 this["Portfolio"]["templates"]["folder"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
@@ -21,4 +21,7 @@ this["Portfolio"]["templates"]["folder"] = Handlebars.template({"compiler":[6,">
   return "<div class=\"folder\">\n  <div class=\"folder__bar\">\n    <p class=\"bar__title\">"
     + this.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
     + "</p>\n    <img class=\"bar__icn bar__icn--expand\" src=\"../data/img/expand.png\"/>\n    <img class=\"bar__icn bar__icn--minimize\" src=\"../data/img/minimize.png\"/>\n  </div>\n  <div class=\"folder__items\">\n    <p></p>\n  </div>\n</div>\n";
+},"useData":true});
+this["Portfolio"]["templates"]["taskbar"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<div class=\"taskbar__start-btn\">\n  <p class=\"start-btn__txt\">Start</p>\n</div>\n";
 },"useData":true});
